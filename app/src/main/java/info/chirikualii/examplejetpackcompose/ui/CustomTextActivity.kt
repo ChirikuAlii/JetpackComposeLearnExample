ca3c55a718ed750bad8e1ebbec94cde45399fe53
ChirikuAlii <chiriku.ali.rhmat@gmail.com>
2020-07-28 15:53:40 +0700

    add custom text page

---
+++ app/src/main/java/info/chirikualii/examplejetpackcompose/ui/CustomTextActivity.kt
@@ -0,0 +1,10 @@
+package info.chirikualii.examplejetpackcompose.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
